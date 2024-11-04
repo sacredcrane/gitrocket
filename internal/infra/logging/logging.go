c66f9ea61fc5f5b8b5a35cde1f70ebe67d1d4db0
sacredcrane <cwfrcpp@gmail.com>
2024-11-04 17:33:18 +0300

    feat: added logging setup func

--- internal/infra/logging/logging.go
+++ internal/infra/logging/logging.go
@@ -1,1 +1,57 @@
 package logging
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envProd  = "production"
+	envDev   = "development"
+	envLocal = "local"
+)
+
+func Err(err error) slog.Attr {
+	return slog.Attr{
+		Key:   "Error",
+		Value: slog.StringValue(err.Error()),
+	}
+}
+
+func SetupLogger(envType string) (log *slog.Logger) {
+	switch envType {
+	case envProd:
+		{
+			log = slog.New(
+				slog.NewJSONHandler(
+					os.Stdout, &slog.HandlerOptions{
+						Level: slog.LevelWarn,
+					},
+				),
+			)
+		}
+	case envDev:
+		{
+			log = slog.New(
+				slog.NewJSONHandler(
+					os.Stdout, &slog.HandlerOptions{
+						Level: slog.LevelInfo,
+					},
+				),
+			)
+		}
+	case envLocal:
+		fallthrough
+	default:
+		{
+			log = slog.New(
+				slog.NewTextHandler(
+					os.Stdout, &slog.HandlerOptions{
+						Level: slog.LevelDebug,
+					},
+				),
+			)
+		}
+	}
+	return
+}
